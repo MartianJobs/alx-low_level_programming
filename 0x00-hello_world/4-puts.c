b37127cc43f3be5cd83d7785df83a112f21738e2
Martian Jobs <eyobbasazinew@gmail.com>
2023-02-16 13:12:58 -0800

    Printf and compiler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A C that prints exact input
+ * Return: 0 (Success)
+ * /
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
