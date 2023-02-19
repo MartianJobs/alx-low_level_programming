4f7c3e7ae89e96b98f8764c09b2c08276a1048d8
Martian Jobs <eyobbasazinew@gmail.com>
2023-02-19 05:54:48 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - A program that print a line using the printf function
- * Return: 0 (Success)
- * /
-int main (void)
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	printf("with proper grammer but bt theoutcome is a work of art,\n")
 	return (0);
