1dd0f5a9d11193db21f1c0c4d843c72c91278694
Martian Jobs <eyobbasazinew@gmail.com>
2023-03-06 07:23:03 -0800

    Updated

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,18 +1,30 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
  */
-int main(void)
-{
-    char *s = "hello, world";
-    char *f = "oleh";
-    unsigned int n;
 
-    n = _strspn(s, f);
-    printf("%u\n", n);
-    return (0);
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, n, value, check;
+
+	value = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		check = 0;
+
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (accept[n] == s[i])
+			{
+				value++;
+				check = 1;
+			}
+		}
+	}
 }
+
