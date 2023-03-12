428968184ad2a3f55a1fb28590580f3fe53f9ffa
Martian Jobs <eyobbasazinew@gmail.com>
2023-03-11 23:39:04 -0800

    Updated

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <string.h>
 
-int main (int argc, char *argv[]);
-int atoi(char *s);
+int main(int argc, char *argv[]);
+int _atoi(char *s);
+int _strlen(char *s);
 
 #endif
